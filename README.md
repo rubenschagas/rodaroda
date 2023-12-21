b10a22bd3e8009da4f81ff73e3e742a07041233c
rubenschagas <rubens.chagas@gmail.com>
2023-12-20 23:46:42 -0300

    :refactor: (core): creating the project.

---
+++ README.md
@@ -0,0 +1,77 @@
+# RODARODA - SIMPLY A CRUD POC
+
+## TABLE OF CONTENTS
+
+[OVERVIEW](#overview)
+
+[PREREQUISITES](#prerequisites)
+
+[USE CASES](#use-cases)
+
+[SENDING DATA TO THE DATABASE THROUGH THE BACKEND SERVICE](#sending-data-to-the-database-through-the-backend-service)
+
+## OVERVIEW
+
+The objective of this README.md document file is to provide help on how to run the automated deployment of a [local] CRUD project, as a Proof of Concept (POC), running a backend service.
+
+A CRUD Proof of Concept (POC) serves as a foundational demonstration showcasing the essential operations of Create, Read, Update, and Delete within a software application or system. This pivotal phase not only validates the technical feasibility of these core functionalities but also lays the groundwork for the entire application's success.
+
+The Create operation initiates the system's ability to generate new data, fostering an environment for innovation and growth. Reading data ensures seamless access to information, promoting efficiency and informed decision-making. Updates reflect the system's adaptability, allowing for real-time adjustments and improvements. Deletion, while seemingly straightforward, underscores the importance of data management and integrity.
+
+Beyond its technical implications, a CRUD POC symbolizes the inception of a robust and scalable application. It acts as a tangible representation of the development team's prowess, showcasing their ability to translate conceptual ideas into functional realities. This proof of concept is not merely a technical exercise but a strategic step towards building a reliable and user-centric solution.
+
+In the broader context of software development, a well-executed CRUD POC sets the stage for further feature enhancements, integrations, and overall system evolution. Its successful implementation not only assures stakeholders of the project's viability but also instills confidence in the development process.
+
+As developers navigate the intricacies of the CRUD POC, they pave the way for a comprehensive and sophisticated application that aligns with the needs of users and stakeholders alike. In essence, the CRUD POC is not just a technical milestone but a testament to the commitment to excellence in software development.
+
+## PREREQUISITES
+
+```
+1. "docker": "24.0.2";
+2. "docker compose": "2.18.1";
+3. a postgres dbms server, as found in the https://github.com/rubenschagas/postgres project;
+4. run the ansible playbook rodaroda.yml from the https://github.com/rubenschagas/ansibleAutomatedPlaybooks project.
+```
+
+## USE CASES
+
+Start the server:
+
+Make sure the Node.js server is running. 
+
+You can start the server by executing the following command in the terminal within the project directory:
+
+```
+node index.js
+```
+
+## SENDING DATA TO THE DATABASE THROUGH THE BACKEND SERVICE
+
+It is possible to send requests through Postman to test the CRUD endpoints created in the previous example. 
+
+Here are some steps to test the GET and POST endpoints for the `localidade` entity:
+
+Open the Postman application.
+
+Send a GET request to list localities:
+
+Select the GET method.
+Enter the URL: http://localhost:3000/localidades.
+Click "Send" to submit the request.
+Send a POST request to create a locality:
+
+Select the POST method.
+Enter the URL: http://localhost:3000/localidades.
+Go to the "Body" tab and select the raw format.
+Insert the locality data in the request body, json, for example:
+
+```
+{
+"nome": "Localidade Teste",
+"tipo": "origem"
+}
+```
+
+Click "Send" to submit the request.
+
+![](./assets/readMeMd/env.png)
---
+++ index.js
@@ -0,0 +1,151 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.json());
+
+// Configuração do pool de conexão com o PostgreSQL
+const pool = new Pool({
+  host: 'localhost',
+  database: 'rodaroda',
+  user: 'postgres',
+  password: 'postgres',
+  port: 5432,
+});
+
+// CRUD para Localidade
+
+
+// CRUD para Produto
+app.get('/produtos', async (req, res) => {
+  const result = await pool.query('SELECT * FROM produto');
+  res.json(result.rows);
+});
+
+app.post('/produtos', async (req, res) => {
+  const { nome, descricao } = req.body;
+
+  // Use parameterized query to handle quotes
+  const result = await pool.query('INSERT INTO produto (nome, descricao) VALUES ($1, $2) RETURNING *', [nome, descricao]);
+
+  res.json(result.rows[0]);
+});
+
+
+app.get('/produtos/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query('SELECT * FROM produto WHERE id = $1', [id]);
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: 'produto não encontrado' });
+  }
+
+  res.json(result.rows[0]);
+});
+
+app.put('/produtos/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nome, descricao } = req.body;
+  const result = await pool.query('UPDATE produto SET nome = $1, descricao = $2 WHERE id = $3 RETURNING *', [nome, descricao, id]);
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: 'produto não encontrado' });
+  }
+
+  res.json(result.rows[0]);
+});
+
+app.delete('/produtos/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query('DELETE FROM produto WHERE id = $1 RETURNING *', [id]);
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: 'produto não encontrado' });
+  }
+
+  res.json({ message: 'produto excluído com sucesso' });
+});
+
+// CRUD para transportadora
+app.get('/transportadoras', async (req, res) => {
+  const result = await pool.query('SELECT * FROM transportadora');
+  res.json(result.rows);
+});
+
+app.post('/transportadoras', async (req, res) => {
+  const { nome, contato } = req.body;
+  const result = await pool.query('INSERT INTO transportadora (nome, contato) VALUES ($1, $2) RETURNING *', [nome, contato]);
+  res.json(result.rows[0]);
+});
+
+app.get('/transportadoras/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query('SELECT * FROM transportadora WHERE id = $1', [id]);
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: 'transportadora não encontrado' });
+  }
+
+  res.json(result.rows[0]);
+});
+
+app.put('/transportadoras/:id', async (req, res) => {
+  const { id } = req.params;
+  const { nome, descricao } = req.body;
+  const result = await pool.query('UPDATE transportadora SET nome = $1, descricao = $2 WHERE id = $3 RETURNING *', [nome, descricao, id]);
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: 'transportadora não encontrado' });
+  }
+
+  res.json(result.rows[0]);
+});
+
+app.delete('/transportadoras/:id', async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query('DELETE FROM transportadora WHERE id = $1 RETURNING *', [id]);
+
+  if (result.rows.length === 0) {
+    return res.status(404).json({ error: 'transportadora não encontrado' });
+  }
+
+  res.json({ message: 'transportadora excluído com sucesso' });
+});
+
+// CRUD para veiculo
+app.get('/veiculos', async (req, res) => {
+  const result = await pool.query('SELECT * FROM veiculo');
+  res.json(result.rows);
+});
+
+app.post('/veiculos', async (req, res) => {
+  const { modelo, placa } = req.body;
+  const result = await pool.query('INSERT INTO veiculo (modelo, placa) VALUES ($1, $2) RETURNING *', [modelo, placa]);
+  res.json(result.rows[0]);
+});
+
+// Adicione rotas semelhantes para atualização, exclusão e visualização individual para veiculo
+
+// CRUD para viagem
+app.get('/viagens', async (req, res) => {
+  const result = await pool.query('SELECT * FROM viagem');
+  res.json(result.rows);
+});
+
+app.post('/viagens', async (req, res) => {
+  const { origem_id, destino_id, produto_id, transportadora_id, veiculo_id, data_partida, data_chegada } = req.body;
+  const result = await pool.query('INSERT INTO viagem (origem_id, destino_id, produto_id, transportadora_id, veiculo_id, data_partida, data_chegada) VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *',
+    [origem_id, destino_id, produto_id, transportadora_id, veiculo_id, data_partida, data_chegada]);
+  res.json(result.rows[0]);
+});
+
+// Adicione rotas semelhantes para atualização, exclusão e visualização individual para viagem
+
+
+// Inicie o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
